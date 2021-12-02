6b04a18ac81722197ab652a9e93870904b373ee9
Peter <warrenp11@gmail.com>
2021-12-02 12:24:31 -0500

    single post displaying

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
 
 // Homepage route
+// Displays all posts
 router.get("/", (req, res) => {
   Post.findAll({
     attributes: ["id", "title", "content", "created_at"],
@@ -37,12 +38,53 @@ router.get("/", (req, res) => {
     });
 });
 
-// Login page
-router.get("/login", (req, res) => {
-    res.render("login")
+// Displays single post
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id",
+        });
+        return;
+      }
+
+      const post = dbPostData.get({
+        plain: true,
+      });
+
+      res.render("single-post", {
+        post,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-
-
+// Login page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = router;
